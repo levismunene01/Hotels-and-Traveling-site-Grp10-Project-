f4fa257a3ee8d2a3d843abc738be580ab852db7a
collins kipngeno <kipngenolins01@gmail.com>
2024-05-07 13:44:05 +0300

    App.jsx,package.json, package-lock.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-
-
+import React from "react";
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
+import Footer from "./Components/Footer";
 function App() {
-  
-
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/destination" component={Destination} />
+          <Route path="/hotels" exact component={HotelList} />
+          <Route path="/hotels/:hotelId" component={HotelDetail} />
+          <Route path="/search" component={Search} />
+          <Route path="/booking" component={Booking} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
