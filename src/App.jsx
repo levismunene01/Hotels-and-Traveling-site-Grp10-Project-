839e11faeaeebb4ed3d83581c9a769e848b68af3
Margaret <mungaimargaretn@gmail.com>
2024-05-09 12:57:45 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
 import Footer from "./Components/Footer";
 import Search from "./Components/Search";
+
 function App() {
   return (
     <Router>
