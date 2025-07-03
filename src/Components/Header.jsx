b4c0f243e6389fd8c33219496b34d30bb0382442
levis <levismunene01@gmail.com>
2025-07-03 14:23:08 +0300

    hearder syting changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,57 +1,55 @@
-import { Flex, Heading, Button } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
-
-
-
-
-
-
-
+import { Flex, Heading, Button } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <Flex
       as="header"
+      direction="column"
       align="center"
-      justify="space-between"
-      py={4}
-      px={8}
+      justify="center"
+      py="4"
+      px="8"
       bg="gray.800"
-      color="black"
-      borderBottom="1px solid #0364e2"
+      color="white"
+      borderBottom="2px solid #3182ce"
+      boxShadow="md"
     >
-      <Heading as="h1" fontSize="2xl" fontWeight={'bold'}>
-        QUICKSAFE TOURS TRAVELINGS AND HOTELS
-      </Heading>
+   <Heading
+  as="h1"
+  fontSize="3xl"
+  fontWeight="bold"
+  color="gray.300"
+  textAlign="center"
+  textShadow="2px 2px #1a202c"
+  mb="4"
+>
+  QuickSafe Tours & Hotels
+</Heading>
+
+
       <Flex>
         <Button
           as={Link}
           to="/about"
           colorScheme="blue"
-          mr={4}
-          _hover={{ transform: 'translateY(-1px)', boxShadow: 'lg' }}
-          _active={{ transform: 'translateY(1px)', boxShadow: 'none' }}
+          mr="4"
         >
           About
         </Button>
+
         <Button
           as={Link}
           to="/contact"
-          colorScheme="blue"
-          _hover={{ transform: 'translateY(-1px)', boxShadow: 'lg' }}
-          _active={{ transform: 'translateY(1px)', boxShadow: 'none' }}
-          style={{
-            background: 'linear-gradient(145deg, #2a2e37, #21242b)',
-            border: '1px solid #2a2e37',
-            borderRadius: '8px',
-            boxShadow: '5px 5px 10px #1a1c20, -5px -5px 10px #333739',
-          }}
+          bg="teal.500"
+          color="white"
+          _hover={{ bg: 'teal.600' }}
         >
           Contact
         </Button>
       </Flex>
     </Flex>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
